b4d511d483861fc2075179c99297f79f7021d047
Nguyễn Minh Huy <quilivh11@gmail.com>
2024-03-19 03:12:33 +0700

    main

---
+++ Sucess.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Done</title>
+</head>
+<style>
+    body{
+        text-align: center;
+        margin-top: 200px;
+    }
+    #back{ 
+        padding: 10px;
+        background-color: bisque;
+        text-decoration: none;
+        color:black;
+        border-radius: 5px;
+        border: 1px solid black;
+    }
+    #back:hover{
+        background-color: coral;
+        color: white;
+       
+    }
+    #back:active{
+        background-color: brown;
+        color: white;
+       
+    }
+</style>
+<body>
+    <h1>
+        Thank you for reset password
+    </h1>
+    <a id="back" href="/php/Homepage.php"> Please Click here to comeback to homepage</a>
+</body>
+</html>
